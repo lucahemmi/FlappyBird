b0d68f3edeed48340316000a644d74448befe8e0
lucahemmi <hemmingerluca@gmail.com>
2022-11-09 11:55:40 +0100

    Hinzufügen von "Character"

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
 
+
 hole.addEventListener('animationiteration',() =>{
     var random = -((Math.random()*300) + 150); 
     hole.style.top = random + "px";
 });
+
 setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
 },10);
+
+ 
