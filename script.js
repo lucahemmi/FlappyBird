f90e4316c0305a6404cf3d759ae96dcf011041b6
lucahemmi <hemmingerluca@gmail.com>
2022-11-23 11:26:55 +0100

    updated jscript dynamisches Leaderboard als nächst

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var hole = document.getElementById("hole");
 var character = document.getElementById("character");
 var jumping = 0;
 var counter = 0;
+
 hole.addEventListener('animationiteration',() =>{
     var random = -((Math.random()*300) + 150); 
     hole.style.top = random + "px";
@@ -14,12 +15,21 @@ setInterval(function(){
     if (jumping == 0) { 
     character.style.top = (characterTop+2)+"px"
     }
-    if (characterTop>560){
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var cTop = -(500-characterTop);
+    if (characterTop>570){
         alert("Game Over. Score: " + counter);
-        character.style.top = 100+"px";
+        character.style.top = 100 +  "px";
+        counter = 0;
+     }
+    if (characterTop<80){
+        alert("Game Over. Score: " + counter);
+        character.style.top = 100 + "px";
         counter = 0;
     }
-
+    
+    
 },10);
 
  function jump() {
@@ -27,14 +37,16 @@ setInterval(function(){
     let jumpCount = 0;
     var jumpInterval = setInterval(function(){
         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        if ((characterTop>6)&&(counter<15)) {
+        if ((characterTop>6)&&(jumpCount<25)) {
             character.style.top = (characterTop-3)+"px";
         }
-            if (jumpCount > 20) {
+        if (jumpCount>20){
                 clearInterval(jumpInterval);
-                jumping = 0;
-                jumpCount = 0;
+                jumping= 0;
+                jumpCount=0;
         }
-
+        jumpCount++; 
     }, 10);
  }
+
+ let value = Number(localStorage.getItem('counter'));
