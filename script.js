3be32d2934514eb76967a48c480fbd3da04b8d84
lucahemmi <hemmingerluca@gmail.com>
2023-01-15 17:49:42 +0100

    Fehlersuche hat bei mir nix ergeben/localStorage?

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
-var inputfeld = document.getElementById("inputspace");
+var inputfeld = document.getElementById("username");
 var jumping = 0;
 var counter = 0;
 
 inputspace.addEventListener('click',() =>{
     
-})
+});
 
 hole.addEventListener('animationiteration',() =>{
     var random = -((Math.random()*300) + 150); 
+    var top = (random*100)+150;
     hole.style.top = random + "px";
     counter++;
 });
@@ -18,16 +19,16 @@ hole.addEventListener('animationiteration',() =>{
 setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if (jumping == 0) { 
-    character.style.top = (characterTop+2)+"px"
+        character.style.top = (characterTop+2)+"px";
     }
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
     var cTop = -(500-characterTop);
-    if (characterTop>570){
+    if ((characterTop>570)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
         alert("Game Over. Score: " + counter);
         character.style.top = 300 +  "px";
         counter = 0;
-     }
+    }
     if (characterTop<80){
         alert("Game Over. Score: " + counter);
         character.style.top = 300 + "px";
@@ -37,7 +38,7 @@ setInterval(function(){
     
 },10);
 
- function jump() {
+function jump() {
     jumping = 1;
     let jumpCount = 0;
     var jumpInterval = setInterval(function(){
@@ -52,13 +53,27 @@ setInterval(function(){
         }
         jumpCount++; 
     }, 10);
- }
+}
 
- let value = Number(localStorage.getItem('counter'));
+ 
+const storageInput = document.querySelector('.storage');
+const text = document.querySelector('.text');
+const button = document.querySelector('.button');
+const storedInput = localStorage.getItem('textinput');
 
- function namensfeld() {
-    var input = document.getElementById("inputspace").value;
-    
-    
-    alert (input);
- }
+if(storageInput) { 
+    text.textContent = storedInput
+}
+
+storageInput.addEventListener('input', letter =>{
+    text.textContent = storageInput.value
+})
+
+const saveInLocal = () => {
+    localStorage.setItem('textinput', text.textContent)
+}
+
+button.addEventListener('click', saveInLocal);
+
+
+
